6751d415cd1767934ebd18a4271598d916a8b588
hyousi <t0iiz@outlook.com>
2024-06-02 17:04:19 +0800

    ci: add test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     watch: false,
+    coverage: {
+      provider: "v8",
+      include: ["src/**"],
+      exclude: ["src/**/index.ts"],
+    },
   },
 });
